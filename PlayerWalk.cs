0b14755129c604211b630219d29f4a1b43d9e238
Bookgirl97 <34253096+Bookgirl97@users.noreply.github.com>
2018-11-01 15:33:21 +0100

    Add files via upload

---
+++ PlayerWalk.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour {
+
+    public int playerSpeed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+        }
+
+    }
+}
